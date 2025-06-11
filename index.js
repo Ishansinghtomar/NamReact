c41cf5bc7e1687bcbf221cc1827e05bfce0fe543
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2025-06-12 00:39:43 +0530

    initial commit

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const a=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{id:"gchild"},"welcome to my world")))
-const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(a);
