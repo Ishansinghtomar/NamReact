95f1a87f9a0b279fbd3ca1a87c64c3ee2585c14c
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2025-08-29 15:44:59 +0530

    changes

--- Episode 5/src/Components/Body.js
+++ Episode 5/src/Components/Body.js
@@ -1,13 +1,21 @@
 import ResContainer from "./ResContainer.js";
 import Data from "../Utils/data.js"
+
 const Body=()=>
-{
+{ let top=Data;
     return(
 <div className="body">
-    <div className="Search">Search</div>
+    <div className="filter-btn">
+        <button onClick={()=>{
+            top=Data.filter((a)=> a?.info?.avgRating >= 4)
+
+        }
+    } 
+        >Filter Top Rated</button>
+    </div>
     <div className="res-container">
         <ResContainer resData={Data[0]}/> 
-        {Data.map((resturarnt)=>(
+        {top.map((resturarnt)=>(
             <ResContainer resData={resturarnt} key={resturarnt?.info?.id}/>  
         ))}
 
--- Episode 5/src/Components/ResContainer.js
+++ Episode 5/src/Components/ResContainer.js
@@ -5,7 +5,6 @@ const ResContainer=(props)=>{
     const {name,costForTwo}=props?.resData?.info
     return(
 <div className="res-card">
-    {console.log(props)}
     <img src={CDN_URL + props?.resData?.info?.cloudinaryImageId} ></img>
     <h5>{name}</h5>
     <h6>{props?.resData?.info?.cuisines.join(",")}</h6>
