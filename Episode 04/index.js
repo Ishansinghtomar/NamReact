af5d9a72ec454eb80579f2a91f4092aa3e9a5796
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2026-01-25 01:14:07 +0530

    Episode 04

---
+++ Episode 04/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import resdata from "./data";
+
+
+const Header=()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img  className="img" src="https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/83/51/f0/8351f06c-2826-69bf-1cd5-3d501d1b2a3e/iMessage_App_Icon-1x_U007emarketing-0-0-85-181-0.png/1200x630wa.jpg" alt="Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+                </ul>
+                </div>
+                </div>
+
+    )
+}
+const RestCard=(props)=>{
+    const {data}=props;
+    const {name,avgRating,cuisines,costForTwo,sla}=data?.info
+    return (
+                <div className="res-card" style={{ backgroundColor: "#f0f0f0f0" }}>
+                    <div className="logo1">
+                        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+data.info.cloudinaryImageId} alt="" />
+                    </div>
+                    <h3>{name}</h3>
+                    <h4>{avgRating}</h4>
+                    <h4>{cuisines.join(",")}</h4>
+                    <h4>{costForTwo}</h4>
+                    <h4>{sla.slaString}</h4>
+                </div>
+    )
+}
+
+const Body=()=>{
+    return (
+        <div className="body">
+            <div className="Search">Search </div>
+            <div className="res-container">
+           {
+            resdata.map((resturarant)=>
+                <RestCard data={resturarant} key={resturarant.info.id} ></RestCard>
+
+           )
+           }
+               
+
+            </div>
+
+        </div>
+    )
+}
+const AppLayout=()=>{
+    return (
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+    )
+}
+
+const Root=ReactDOM.createRoot(document.getElementById("root"))
+Root.render(<AppLayout/>)
