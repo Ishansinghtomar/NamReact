63a2cce4f495367b42c31ee5dd5c685d691eb064
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2026-01-24 11:22:31 +0530

    Episode 02

---
+++ Episode 02/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading=React.createElement("h1",{},"Hi Ishan")
+const Root=ReactDOM.createRoot(document.getElementById("root"))
+Root.render(heading)
