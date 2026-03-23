1d20b1c8d89b70e8506156513f545799e43bffad
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2026-03-23 07:00:53 +0530

    about us

--- Episode 08/src/Components/About.js
+++ Episode 08/src/Components/About.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import FunctionCom from './AboutsubComponent/FunctionComp'
+import ClassComp from './AboutsubComponent/ClassComp'
 
 const About = () => {
   return (
-    <div>AboutUs</div>
+    <div>AboutUs
+    <FunctionCom name={"Ishan"} Contact={"Ishaitis@hmail.com "}/>
+    <ClassComp name={"Ishan"} Contact={"Ishaitis@hmail.com "}/>
+    </div>
   )
 }
 
--- Episode 08/src/Components/Contact.js
+++ Episode 08/src/Components/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Parent1 from './ClassParentChild/Parent1'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>Contact
+    <Parent1></Parent1>
+    </div>
   )
 }
 
